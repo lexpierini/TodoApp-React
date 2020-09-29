3e2990bacd3377ff4c6a6079e31653640526c8b4
Alex Pierini <alex_pierini@hotmail.com>
2020-09-29 17:07:46 -0400

    Composant PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default (props) => (
   <div>
-    <h1>À propos</h1>
+    <PageHeader name="À propos" small="Nous" />
+
+    <h2>Notre Histoire</h2>
+    <p>Lorem ispsum dolor sit amet...</p>
+    <h2>Mission et Vision</h2>
+    <p>Lorem ispsum dolor sit amet...</p>
+    <h2>Presse</h2>
+    <p>Lorem ispsum dolor sit amet...</p>
   </div>
 );
