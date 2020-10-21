c031e11adcc3e11a0ebce1effa3eb593166cd7e5
Alex Pierini <alex_pierini@hotmail.com>
2020-10-21 19:10:21 -0400

    Application d'un Middleware (Redux-Promise)

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,18 +1,6 @@
 const INITIAL_STATE = {
-    description: "Lire un livre de français",
-    list: [{
-        _id: 1,
-        description: "Payer la facture de la carte de crédit",
-        done: true,
-    }, {
-        _id: 2,
-        description: "Rendez-vous avec l'équipe à 10h",
-        done: false,
-    }, {
-        _id: 3,
-        description: "Rendez-vous médical le mardi après le déjeuner",
-        done: false,
-    }]
+    description: "",
+    list: [],
 }
 
 export default (state = INITIAL_STATE, action) => {
